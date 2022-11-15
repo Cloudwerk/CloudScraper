47c34979bab361cab300163d305c312df72fa242
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2022-11-15 09:53:06 +0100

    Added sorting by clicking on col header #2 and linked the sites in list #6

--- cloudscraper/src/View/RenderSitesList.tsx
+++ cloudscraper/src/View/RenderSitesList.tsx
@@ -5,16 +5,18 @@ import { IRenderSitesListProps } from '../Model/Interfaces/IRenderSitesListProps
 
 export const RenderSitesList = (props: IRenderSitesListProps) => {
     let _columns: IColumn[];
+    let prevColumn: IColumn;
     const appContext = useContext(AppContext)
 
 
+
     _columns = [
-        { key: 'name', name: 'Name', fieldName: 'SiteName', minWidth: 100, maxWidth: 300, isResizable: true, onColumnClick: Sort, },
-        { key: 'webUrl', name: 'URL', fieldName: 'Url', minWidth: 100, maxWidth: 350, isResizable: true, onColumnClick: Sort}, // Add sorting properties
+        { key: 'name', name: 'Name', fieldName: 'SiteName', minWidth: 100, maxWidth: 300, isResizable: true, onColumnClick: Sort, isSorted: false, isSortedDescending: false,},
+        { key: 'webUrl', name: 'URL', fieldName: 'Url', minWidth: 100, maxWidth: 350, isResizable: true, onColumnClick: Sort, onRender: onRenderLink, isSorted: false, isSortedDescending: false}, // Add sorting properties
         { key: '', name: 'Owner', fieldName: 'SiteOwner', minWidth: 150, maxWidth: 200, isResizable: true, onColumnClick: Sort},
-        { key: 'Description', name: 'Description', fieldName: 'Description', minWidth: 150, maxWidth: 350, isResizable: true, onColumnClick: Sort},
-        { key: 'lastModifiedDateTime', name: 'Date Modified', fieldName: 'DateModified', minWidth: 100, maxWidth: 300, isResizable: true, onColumnClick: Sort},
-        { key: 'createdDateTime', name: 'Date Created', fieldName: 'DateCreated', minWidth: 100, maxWidth: 350, isResizable: true, onColumnClick: Sort}
+        { key: 'Description', name: 'Description', fieldName: 'Description', minWidth: 150, maxWidth: 350, isResizable: true, onColumnClick: Sort, isSorted: false, isSortedDescending: false},
+        { key: 'lastModifiedDateTime', name: 'Date Modified', fieldName: 'DateModified', minWidth: 100, maxWidth: 300, isResizable: true, onColumnClick: Sort, isSorted: false, isSortedDescending: false},
+        { key: 'createdDateTime', name: 'Date Created', fieldName: 'DateCreated', minWidth: 100, maxWidth: 350, isResizable: true, onColumnClick: Sort, isSorted: false, isSortedDescending: false}
         ];
 
     return (
@@ -31,13 +33,23 @@ export const RenderSitesList = (props: IRenderSitesListProps) => {
     )
         
     function Sort(ev: React.MouseEvent<HTMLElement>, column: IColumn): void {
-        if (column.key !== appContext.appContext.sortArgs) {
+        if (prevColumn !== column) {
+            prevColumn.isSorted = false;
+            prevColumn.isSortedDescending = false;
+            prevColumn = column;
+        }
+
+        if (column.isSorted === false && column.isSortedDescending === false) {
+            column.isSorted = true;
             appContext.appContext.sortArgs = ("&$orderBy=" + column.key);
         }
-        else if (column.key === appContext.appContext.sortArgs) {
+        else if (column.isSorted === true) {
+            column.isSorted = false;
+            column.isSortedDescending = true;
             appContext.appContext.sortArgs = ("&$orderBy=" + column.key + " desc");
         }
-        else {
+        else if (column.isSortedDescending === true) {
+            column.isSortedDescending = false;
             appContext.appContext.sortArgs = ("");
         }
 
@@ -45,6 +57,7 @@ export const RenderSitesList = (props: IRenderSitesListProps) => {
     }
 
     function onRenderLink(item: any, index: any, column: any) {
-        return <a href="" />
+        const fieldContent = item[column.fieldName];
+        return <Link href={fieldContent} target="_blank">{fieldContent}</Link>
     }
 }
