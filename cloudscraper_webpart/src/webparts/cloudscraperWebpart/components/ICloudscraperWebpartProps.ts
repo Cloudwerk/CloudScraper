543f9d5a842416454c72b62358c78f442bd47d02
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2022-11-21 16:17:25 +0100

    Added webpart files for SharePoint

---
+++ cloudscraper_webpart/src/webparts/cloudscraperWebpart/components/ICloudscraperWebpartProps.ts
@@ -0,0 +1,7 @@
+export interface ICloudscraperWebpartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
