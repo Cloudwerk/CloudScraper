b1d8c6f6196badcec3c362b8350f8cf2e4b63f75
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2022-11-24 16:49:36 +0100

    Unit test: request to graph

--- cloudscraper/src/Model/RequestSites.test.ts
+++ cloudscraper/src/Model/RequestSites.test.ts
@@ -1,8 +1,10 @@
 import { IColumn } from '@fluentui/react';
 import 'jest';
+import { graphConfig } from '../authConfig';
 import { AppServices } from './AppServices';
 import graphResponseMockData from './mockData';
 import { decodeJSON } from './RequestSites';
+import fetch from 'node-fetch';
 
 test('check number of list values after json decoding', () => {
     const prevAmount: number = graphResponseMockData.length;
@@ -22,3 +24,36 @@ test('correct number of sites after loading more results and sorting', () => {
 
     expect(appServices.loadCounter).toEqual(initialCounterValue + loadCounterIncrease);
 });
+
+// Does currently not work :(
+    // => if thrown away, uninstall node-fetch
+// test('request to graph works', async () => {
+//     const appServices = new AppServices();
+//     const headers: any = []
+//     // const bearer = `Bearer ${appServices.userAccessToken}`;
+//     let errorMessage: any;
+
+//     // headers.append("Authorization", bearer);
+//     const options = {
+//         method: "GET",
+//         // headers: headers,
+//     };
+
+//     let graphValues: any[] = []
+//     await fetch(graphConfig.graphEndPoint + appServices.searchArgs + appServices.sortArgs + "&$top=" + `${appServices.amountSites * appServices.loadCounter}`, options)
+//         .then(response => response.json()
+//         .then((response: any) => {
+//             graphValues = response.value;
+//             if (response["@odata.nextLink"]) {
+//                 appServices.nextLink = response["@odata.nextLink"];
+//             } else {
+//                 appServices.nextLink = "";
+//             }
+//         }))
+//         .catch((error) => {
+//             errorMessage = error;
+//         })
+
+//     expect(errorMessage).toContain('401');
+    
+// });
