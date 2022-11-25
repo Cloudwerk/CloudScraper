2c81ada9f1cf08fb8cb3dc87b725ecc6e421f00c
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2022-11-25 12:19:22 +0100

    better naming for tests and changes to test config

--- cloudscraper/src/Model/RequestSites.test.ts
+++ cloudscraper/src/Model/RequestSites.test.ts
@@ -6,14 +6,14 @@ import graphResponseMockData from './mockData';
 import { decodeJSON } from './RequestSites';
 import fetch from 'node-fetch';
 
-test('check number of list values after json decoding', () => {
+test('compare array length before and after json decoding', () => {
     const prevAmount: number = graphResponseMockData.length;
     const newAmount: number = decodeJSON(graphResponseMockData).length;
 
     expect(newAmount).toBe(prevAmount);
 });
 
-test('correct number of sites after loading more results and sorting', () => {
+test('check amount of sites after loading more results and sorting', () => {
     const appServices = new AppServices();
     const testColumn: IColumn = { key: 'lastModifiedDateTime', name: 'Date Modified', fieldName: 'DateModified', minWidth: 150, maxWidth: 200, isResizable: true, showSortIconWhenUnsorted: true, isSorted: appServices.getSorted('lastModifiedDateTime'), isSortedDescending: appServices.getSortedDesc('lastModifiedDateTime')}
     const initialCounterValue: number = appServices.loadCounter
