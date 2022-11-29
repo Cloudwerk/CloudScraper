8f5129060e0b5f0f889c38f2bdd5db80b7c10fe8
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2022-11-29 09:25:01 +0100

    changed url

--- cloudscraper/src/authConfig.ts
+++ cloudscraper/src/authConfig.ts
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
         clientId: 'fa5aa203-252e-40d0-be4d-3ac69c015e83',
         authority:'https://login.microsoftonline.com/common/',
-        redirectUri: 'https://cloudscraper2.z6.web.core.windows.net/'
+        redirectUri: 'https://cloudscraper.z6.web.core.windows.net/'
     },
     cache: {
         cacheLocation: "sessionStorage",
@@ -21,4 +21,4 @@ export const graphConfig = {
     graphEndPoint: "https://graph.microsoft.com/v1.0/sites?search=*"
 };
 
-// https://cloudscraper2.z6.web.core.windows.net/
+// https://cloudscraper.z6.web.core.windows.net/
