4489d257ec679eea78b82adf81189d595b903e3e
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2022-11-29 10:58:03 +0100

    added another hook for login

--- cloudscraper/src/Model/TokenFetcher.tsx
+++ cloudscraper/src/Model/TokenFetcher.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { loginRequest } from "../authConfig";
-import { useMsal, useMsalAuthentication } from "@azure/msal-react/";
+import { useMsalAuthentication } from "@azure/msal-react/";
 import { InteractionType, InteractionRequiredAuthError } from '@azure/msal-browser';
 import { AppContext } from "./Context/AppContext";
 import { useContext } from "react";
 import { RequestSites } from "./RequestSites";
 import * as msTeams from '@microsoft/teams-js';
+import { useTeams } from "msteams-react-base-component";
 import { useEffect } from "react";
 
 export interface ITokenFetcherProps {
@@ -14,9 +15,22 @@ export interface ITokenFetcherProps {
 
 export const TokenFetcher = () => {
     const appContext = useContext(AppContext).appContext;
+    const [{ inTeams }] = useTeams();
     const { login, result, error } = useMsalAuthentication(InteractionType.Silent);
 
     useEffect(() => {
+        if (inTeams === true) {
+            msTeams.app.initialize();
+            let authTokenRequest = {
+                claims: loginRequest.scopes,
+                silent: false
+            };
+
+            let accessToken = msTeams.authentication.getAuthToken(authTokenRequest);
+            console.log("accessToken values: " + accessToken);
+            appContext.userAccessToken = accessToken.toString();
+        }
+
         if (error instanceof InteractionRequiredAuthError) {
             login(InteractionType.Popup, loginRequest);
         };
@@ -25,24 +39,7 @@ export const TokenFetcher = () => {
             appContext.userAccessToken = result.accessToken
             RequestSites(appContext)
         }
-    }, [error, result]);
-
-    if (appContext.userAccessToken === "" && result) {
-        appContext.userAccessToken = result.accessToken;
-        RequestSites(appContext);
-    }
-
-    // if (appContext.userAccessToken === "") {
-    //     instance
-    //     .acquireTokenSilent({
-    //         ...loginRequest,
-    //         account: accounts[0],
-    //     })
-    //     .then(authResult => {
-    //         appContext.userAccessToken = authResult.accessToken;
-    //         RequestSites(appContext);
-    //     });
-    // }
+    }, [error, result, inTeams]);
 
     return (
         <>
