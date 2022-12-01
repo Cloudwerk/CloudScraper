0478494707fe2df4378861d6fa68a85bdc58ddf5
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2022-12-01 13:27:47 +0100

    Removed unneccessary libraries

--- cloudscraper/src/App/CloudScraperApp.tsx
+++ cloudscraper/src/App/CloudScraperApp.tsx
@@ -9,20 +9,18 @@ import { AppContext } from "../Model/Context/AppContext";
 import { PagingButton } from "../View/PagingButton";
 import { SearchComponents } from "../View/SearchComponents";
 import React from "react";
-import { useTeams } from "msteams-react-base-component";
 
 const appServices = new AppServices();
 
 export const CloudScraperApp = () => {
   const isAuthenticated = useIsAuthenticated();
-  const [{ inTeams }] = useTeams();
   const sitesList = useObservable(appServices.sitesList);
   
     return (
       <div>
         <AppContext.Provider value={{appContext: appServices}}>
           <TokenFetcher />
-          { inTeams ? null : isAuthenticated ? <SignOutButton /> : <SignInButton /> }
+          { isAuthenticated ? <SignOutButton /> : <SignInButton /> }
             <SearchComponents />
             <RenderSitesList sitesList={sitesList} />
             { (appServices.nextLink !== "") ? <PagingButton /> : null }
